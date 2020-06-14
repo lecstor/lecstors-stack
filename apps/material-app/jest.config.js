53b562aec65b135ac9094f29c37c29b24d75a24e
Jason Galea <jason@lecstor.com>
2020-06-14 21:15:42 +1000

    add new react app * material-ui * initial groups/privileges

--- apps/material-app/jest.config.js
+++ apps/material-app/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   moduleDirectories: ["node_modules", "test"],
   coveragePathIgnorePatterns: ["node_modules", "test"],
   setupFilesAfterEnv: ["./test/setup.ts"],
-  testRegex: "(/(\\.|/)(test))\\.[jt]sx?$"
+  testRegex: "(/(\\.|/)(test))\\.[jt]sx?$",
 };
--- apps/react-app/jest.config.js
+++ apps/react-app/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleDirectories: ["node_modules", "test"],
   coveragePathIgnorePatterns: ["node_modules", "test"],
-  setupFilesAfterEnv: ["./test/setup.ts"]
+  setupFilesAfterEnv: ["./test/setup.ts"],
+  testRegex: "(/(\\.|/)(test))\\.[jt]sx?$",
 };
